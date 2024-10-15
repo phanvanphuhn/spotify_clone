9dd5d9e057a8a622c97c2fa96f5e866381414593
Jeff Phan <nostarwhere.pc@gmail.com>
2024-10-15 14:52:40 +0700

    init swift uikit

---
+++ spotify-clone/spotify-clone/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  spotify-clone
+//
+//  Created by VTIT on 15/10/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
