8daa8b9be3817d9c3c88340245bd41ab697a00ef
Jeff Phan <nostarwhere.pc@gmail.com>
2024-10-15 14:52:40 +0700

    init swift uikit

---
+++ spotify-clone/spotify-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spotify-clone
+//
+//  Created by VTIT on 15/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
